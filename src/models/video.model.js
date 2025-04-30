9801dc66e5059bde79c6b2bc0db6c74ee9371168
tariq <rahee.programmer@gmail.com>
2025-04-30 13:08:46 +0500

    learn user and video model with hooks and JWT

---
+++ src/models/video.model.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema({
+
+},
+{timestamps: true});
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
